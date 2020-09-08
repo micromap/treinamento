b3ed6e64829daa1728a9483a139e764d02b47558
Usuario <Usuario@THIAGO-PC>
2020-09-08 17:04:33 -0300

    Testando Commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Ola Git");
+            
+            Console.WriteLine("Testando GIT");
         }
     }
 }
