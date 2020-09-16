8f3efb7ccc18add85d1edfc30007f1c1df9c6527
Usuario <Usuario@ALESSANDRO-PC>
2020-09-15 22:56:54 -0300

    Exercicio Criado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Treinamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá povo de Deus PAI ! VS capeta");
+        }
+    }
+}
