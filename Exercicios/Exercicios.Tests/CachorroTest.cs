21f5d84d568e3d6a1ba8a6fe3c6e5049d8b307db
Rodrigo Sendin Silva <rodrigo@algorama.com.br>
2020-09-16 11:59:19 -0300

    Aula: 013 - Classes e Objetos

---
+++ Exercicios/Exercicios.Tests/CachorroTest.cs
@@ -0,0 +1,39 @@
+﻿using Exercicios.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Exercicios.Tests
+{
+    [TestClass]
+    public class CachorroTest
+    {
+        [TestMethod]
+        public void Cachorro_Latir_Test()
+        {
+            Cachorro leia = new Cachorro();
+            string latido = leia.Latir();
+
+            Console.WriteLine(latido);
+
+            Assert.AreEqual("Au! Au!", latido);
+        }
+
+        // Considerando que a Léia pesa 1Kg,
+        // e come 5% do seu peso de ração,
+        // implemente o Método "QuantoDevoComer"
+        // para passar nesse Teste
+
+        [TestMethod]
+        public void Leia_QuantoDevoComer_Test()
+        {
+            Cachorro leia = new Cachorro();
+            string quantoDevoComer = leia.QuantoDevoComer(1);
+
+            Console.WriteLine(quantoDevoComer);
+
+            Assert.AreEqual("Como tenho 1kg, devo comer 50g por dia", quantoDevoComer);
+        }
+
+        // Criar os métodos de teste, para a Tequila e para o Yuri
+    }
+}
