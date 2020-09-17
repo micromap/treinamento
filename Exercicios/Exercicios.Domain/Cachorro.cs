e77d32aa2fb3541c68d380558ef12acd48c3824f
alexandreavg <40585436+alexandreavg@users.noreply.github.com>
2020-09-17 11:33:20 -0300

    013 - Classes e Objetos

---
+++ Exercicios/Exercicios.Domain/Cachorro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicios.Domain
+{
+    public class Cachorro
+    {
+
+        //Metodo criado pelo VS a partir do auto sugestao.
+        public string Latir()
+        {
+            return "Au! Au!";
+        }
+
+        public double QuantoDevoComer(int peso, double porcIdeal)
+        {
+            return peso * (1+(porcIdeal/100));
+        }
+        
+    }
+}
---
+++ Exercicios/Exercicios.Tests/CachorroTest.cs
@@ -0,0 +1,57 @@
+﻿using Exercicios.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Exercicios.Tests
+{
+    [TestClass]
+    public class CachorroTest
+    {
+
+        [TestMethod]
+        public void Cachorro_Latir_Test()
+        {
+
+            Cachorro leia = new Cachorro();
+            string latido = leia.Latir();
+
+            Console.WriteLine(latido);
+
+            Assert.AreEqual("Au! Au!", latido);
+        }
+
+        //Considerando que leia pesa 1Kg
+        //e come 5% do seu peso em racao
+        //implemente o metodo "QuantoDevoComer"
+        //para passar nesse teste
+        [TestMethod]
+        public void Cachorro_QuantoDevoComer_Test()
+        {
+
+            double percentualCorreto = 5;
+
+            Cachorro leia = new Cachorro();
+            double qtdIdeal = leia.QuantoDevoComer(1, percentualCorreto);
+
+            //Teste passando de parametro somente valores corretos de acordo com a regra
+            //Retorna que funcionou!
+            Assert.AreEqual(leia.QuantoDevoComer(1,5), qtdIdeal);
+
+            //---------------------------------------------------------------------------------------
+
+            Cachorro tequila = new Cachorro();
+            qtdIdeal = tequila.QuantoDevoComer(12, percentualCorreto);
+
+            Assert.AreEqual(tequila.QuantoDevoComer(12, 5), qtdIdeal);
+
+            //---------------------------------------------------------------------------------------
+
+            Cachorro yury = new Cachorro();    
+            qtdIdeal = yury.QuantoDevoComer(12, percentualCorreto);
+
+            Assert.AreEqual(yury.QuantoDevoComer(12, 5), qtdIdeal);
+
+
+        }
+    }
+}
