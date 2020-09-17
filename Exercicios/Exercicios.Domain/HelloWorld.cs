13124d39c393a986949777ff282698bc93116c74
Camila Cardozo <cahcdz@gmail.com>
2020-09-17 11:15:32 -0300

    aula.12

---
+++ Exercicios/Exercicios.Domain/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicios.Domain
+{
+    public static class HelloWorld
+    {
+        public static string SayHello()
+        {
+            return "Hello World!";
+        }
+    }
+}
