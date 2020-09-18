ad46527ed694fd4f227b7fc4d7fc0695dcbd571c
Usuario <Usuario@THIAGO-PC>
2020-09-18 09:20:45 -0300

    Criação ConsoleApp1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Git");
+            
+            Console.WriteLine("Testando GIT");
+
+            Console.WriteLine("Testando Teste");
+        }
+    }
+}
