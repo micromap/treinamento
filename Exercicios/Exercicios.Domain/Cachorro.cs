82f984fc644f0aa37a9376de6671d486b4c7b161
matheus@gemmap.com.br <matheus@gemmap.com.br>
2020-09-18 14:56:19 -0300

    Exercio Cachorro

---
+++ Exercicios/Exercicios.Domain/Cachorro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercicios.Domain
+{
+    public class Cachorro
+    {
+        public string Latir()
+        {
+            return "Au! Au!";
+        }
+
+        public string QuantoDevoComer(int peso)
+        {
+            return $"Como tenho {peso}kg, devo comer {peso*0,05}g por dia";
+        }
+    }
+}
