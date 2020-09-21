f97e12ab4d66e3b91d8a21ecc820263c2537ec16
VINICIUSMASSAFERA <viniciusmassafera@gemmap.com.br>
2020-09-21 14:24:43 -0300

    Aula Sergio Delfino

---
+++ Aula1/Aula1/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Aula1
+{
+    class Produto
+    {
+
+        public int codigo;
+        public string nome;
+        public double preco;
+        public bool ativo;
+        public DateTime data;
+        public Categoria categoria;
+    }
+}
