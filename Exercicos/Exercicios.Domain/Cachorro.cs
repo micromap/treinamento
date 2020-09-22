33d445f5e99084f0acfdcdf206d0d12ea8d6da2d
Usuario <Usuario@THIAGO-PC>
2020-09-22 08:34:51 -0300

    Implementação Método "QuantoDevoComer" Classe Cachorro

---
+++ Exercicos/Exercicios.Domain/Cachorro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Exercicios.Domain
+{
+    public class Cachorro
+    {
+        public string latir()
+        {
+            return "Au! Au!";
+        }
+
+        public double quantoDevoComer(int peso)
+        {
+            double result = ((peso * 100) * 5 / 100);
+
+            return result;
+        }
+    }
+}
---
+++ Exercicos/Exercicios.Test/CachorroTest.cs
@@ -0,0 +1,62 @@
+﻿using Exercicios.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Exercicios.Test
+{   
+    [TestClass]
+    public class CachorroTest
+    {   [TestMethod]
+        public void Cachorro_Latir_Test()
+        {
+            Cachorro leia = new Cachorro();
+            string latido = leia.latir();
+
+            Console.WriteLine(latido);
+
+            Assert.AreEqual("Au! Au!", latido);
+        }
+
+        [TestMethod]
+        public void Leia_QuantoDevoComer_Test()
+        {
+            Cachorro leia = new Cachorro();
+            int peso = 1;
+            double quandoDevoComer = leia.quantoDevoComer(peso);
+
+            double result = ((peso * 1000) * 5 / 100);
+
+            Console.WriteLine("Como tenho " + peso + " KG, devo comer " + quandoDevoComer + "g");
+
+            Assert.AreEqual(result, quandoDevoComer);
+        }
+
+        [TestMethod]
+        public void Tequila_QuandoDevoComer_Teste ()
+        {
+            Cachorro tequila = new Cachorro();
+            int peso = 10;
+            double quandoDevoComer = tequila.quantoDevoComer(peso);
+
+            double result = ((peso * 1000) * 5 / 100);
+
+            Console.WriteLine("Como tenho " + peso + " KG, devo comer " + quandoDevoComer + "g");
+
+            Assert.AreEqual(result, quandoDevoComer);
+        }
+
+        [TestMethod]
+        public void Yuri_QuandoDevoComer_Test()
+        {
+            Cachorro yuri = new Cachorro();
+            int peso = 25;
+            double quandoDevoComer = yuri.quantoDevoComer(peso);
+
+            double result = ((peso * 1000) * 5 / 100);
+
+            Console.WriteLine("Como tenho " + peso + " KG, devo comer " + quandoDevoComer + "g");
+
+            Assert.AreEqual(result, quandoDevoComer);
+        }
+    }
+}
