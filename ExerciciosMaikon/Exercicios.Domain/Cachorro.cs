a00c196b25802756a0649731f8c4b873ab11d5d8
Maikon Frandina <maikon@gemmap.com.br>
2020-09-22 16:24:20 -0300

    013 - Classes e Objetos

---
+++ ExerciciosMaikon/Exercicios.Domain/Cachorro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicios.Domain
+{
+    public class Cachorro
+    {
+        public string Latir()
+        {
+            return "Au! Au!";
+        }
+
+        public string QuantoDevoComer(int peso)
+        {
+            double racao = (peso * 1000) * 0.05;
+
+            return "Como tenho " + peso + "kg, devo comer " + racao + "g por dia";
+            
+        }
+    }
+}
