ad70516ac9b9bd7157e0f5f649f5a15c979c2f96
Usuario <Usuario@THIAGO-PC>
2020-09-23 17:25:48 -0300

    Aula 2 Sergio Delfino, Construtores e Modificadores de Acesso

---
+++ Aula2Sergio/Aula2Sergio/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula2Sergio
+{
+    class Produto
+    {   public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(int codigo, string nome, double preco, int quantidade)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Aula2Sergio/Aula2Sergio/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Aula2Sergio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            produto.Codigo = 10;
+            produto.Nome = "COLA COLA";
+            produto.Preco = 5.8;
+            produto.Quantidade = 2;
+
+            Console.WriteLine("Código     : " + produto.Codigo);
+            Console.WriteLine("Nome       : " + produto.Nome);
+            Console.WriteLine("Preço      : " + produto.Preco);
+            Console.WriteLine("Quantidade : " + produto.Quantidade);
+
+            Console.WriteLine(" ");
+
+            Produto produto1 = new Produto(1,"ARROZ",180.25,1);
+            Console.WriteLine("Código     : " + produto1.Codigo);
+            Console.WriteLine("Nome       : " + produto1.Nome);
+            Console.WriteLine("Preço      : " + produto1.Preco);
+            Console.WriteLine("Quantidade : " + produto1.Quantidade);
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine(produto1.ToString());
+        }
+    }
+}
