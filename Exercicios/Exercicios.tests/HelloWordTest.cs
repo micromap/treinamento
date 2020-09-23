75fc3909fa47ef41fcfe4c8a3b3b0456e9a818fa
luisfelipe@gemmap.com.br <luisfelipe@gemmap.com.br>
2020-09-23 09:01:21 -0300

    Exercício de Hello Word com o Teste

---
+++ Exercicios/Exercicios.tests/HelloWordTest.cs
@@ -0,0 +1,20 @@
+﻿using Exercicios.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicios.tests
+{
+    [TestClass]
+    public class HelloWordTest
+    {
+        [TestMethod]
+        public void SayHello_Test()
+        {
+            string mensagem = HelloWorld.SayHello();
+            Assert.AreEqual("Hello World!", mensagem);
+            Console.WriteLine(mensagem);
+        }
+    }
+}
