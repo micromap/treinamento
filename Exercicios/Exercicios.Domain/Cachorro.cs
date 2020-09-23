3a564661b97d08164ebf81fba62777fabc831d19
DiegoGemmap <diego@gemmap.com.br>
2020-09-23 10:09:56 -0300

    Teste quantidade de ração

---
+++ Exercicios/Exercicios.Domain/Cachorro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicios.Domain
+{
+    public class Cachorro
+    {
+        public string Latir()
+        {
+            return "Au! Au!";
+        }
+
+        public string QuantoDevoComer(int peso)
+        {
+            double racao;
+
+            racao = ((peso * 1000) * 5) / 100;
+
+            return "Como tenho " + peso + "kg, devo comer " + racao + "g por dia";
+        }
+    }
+}
---
+++ Exercicios/Exercicios.Tests/CachorroTest.cs
@@ -0,0 +1,36 @@
+﻿using Exercicios.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicios.Tests
+{
+    [TestClass]
+    public class CachorroTest
+    {
+        [TestMethod]
+        public void Cachorro_Latir_Test()
+        {
+            Cachorro leia = new Cachorro();
+            string latido = leia.Latir();
+
+            Console.WriteLine(latido);
+
+            Assert.AreEqual("Au! Au!", latido);
+        }
+
+        [TestMethod]
+        public void Cachorro_QuantoDevoComer_Test()
+        {
+            int peso = 10;
+
+            Cachorro leia = new Cachorro();
+            string quantoDevoComer = leia.QuantoDevoComer(peso);
+
+            Console.WriteLine(quantoDevoComer);
+
+            Assert.AreEqual("Como tenho " + peso + "kg, devo comer 500g por dia", quantoDevoComer);
+        }
+    }
+}
