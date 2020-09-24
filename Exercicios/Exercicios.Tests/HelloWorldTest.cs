4ff2d8222af77448fce53a6f661ecae07eacae0e
Rodrigo Sendin Silva <rodrigo@algorama.com.br>
2020-09-24 11:49:10 -0300

    Aula: 015 - Tipos no C#

--- Exercicios/Exercicios.Tests/HelloWorldTest.cs
+++ Exercicios/Exercicios.Tests/HelloWorldTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Exercicios.Domain;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 
 namespace Exercicios.Tests
 {
@@ -8,11 +9,104 @@ namespace Exercicios.Tests
     public class HelloWorldTest
     {
         [TestMethod]
-        public void Test_SayHello()
+        public void SayHello_Test()
         {
             var mensagem = HelloWorld.SayHello();
             Console.WriteLine(mensagem);
             Assert.AreEqual("Hello World!", mensagem);
+        }
+
+        [TestMethod]
+        public void Tipos_Primitivos_e_Complexos_Test()
+        {
+            // Tipos Primitivos
+            string mensagem = "Hello World!";
+            double peso = 1.2;
+            int idade = 4;
+
+            Console.WriteLine(mensagem);
+            Console.WriteLine(peso);
+            Console.WriteLine(idade);
+
+            //Tipo complexo
+            Cachorro leia = new Cachorro();
+            Console.WriteLine(leia);
+        }
+
+        [TestMethod]
+        public void Conversao_Implicita_Test()
+        {
+            int inteiro = 10;
+            double valor = inteiro;
+
+            Console.WriteLine(valor);
+            Assert.AreEqual(inteiro, valor);
+        }
+
+        [TestMethod]
+        public void Conversao_Explicita_Test()
+        {            
+            double valor = 1.23;
+            int inteiro = (int)valor;
+
+            Console.WriteLine(inteiro);
+            Assert.AreNotEqual(valor, inteiro);
+        }
+
+        [TestMethod]
+        public void Tipo_de_Valor_Test()
+        {
+            int valor = 10;
+            HelloWorld.PassaValor(valor);
+
+            Console.WriteLine(valor);
+            Assert.AreEqual(10, valor);
+        }
+
+        [TestMethod]
+        public void Tipo_de_Referencia_Test()
+        {
+            Cachorro cachorro = new Cachorro();
+            cachorro.SetNome("Léia");
+
+            HelloWorld.PassaReferencia(cachorro);
+
+            string nomeCachorro = cachorro.GetNome();
+
+            Console.WriteLine(nomeCachorro);
+            Assert.AreEqual("Tequila", nomeCachorro);
+        }
+
+        [TestMethod]
+        public void Igualdade_entre_Tipos_de_Valor_Test()
+        {
+            int valor1 = 10;
+            int valor2 = 10;
+
+            Assert.AreEqual(valor1, valor2);
+        }
+
+        [TestMethod]
+        public void Desigualdade_entre_Tipos_de_Referencia_Test()
+        {
+            Cachorro cachorro1 = new Cachorro();
+            cachorro1.SetNome("Léia");
+
+            Cachorro cachorro2 = new Cachorro();
+            cachorro1.SetNome("Léia");
+
+            Assert.AreNotEqual(cachorro1, cachorro2);
+        }
+
+        [TestMethod]
+        public void Igualdade_entre_Tipos_de_Referencia_Test()
+        {
+            Cachorro cachorro1 = new Cachorro();
+            cachorro1.SetNome("Léia");
+
+            Cachorro cachorro2 = cachorro1;
+
+            Assert.AreEqual(cachorro1, cachorro2);
         }
     }
 }
