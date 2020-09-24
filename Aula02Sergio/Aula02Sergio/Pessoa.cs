c710da0b771c0f772903fdbf9facf96517792c13
TamaraAlessandra <tamara@gemmap.com.br>
2020-09-24 17:10:20 -0300

    Classe Pessoa

---
+++ Aula02Sergio/Aula02Sergio/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula02Sergio
+{
+    class Pessoa
+    {
+        public int Codigo { get; set; } //Encapsulamento
+        public string Nome { get; set; } //"Nome"(LETRA MAIUSCULA) é propriedade de nome que dá permissão de acesso - leitura e escrita
+        public double Altura { get; set; }
+    }
+}
+
--- Aula02Sergio/Aula02Sergio/Program.cs
+++ Aula02Sergio/Aula02Sergio/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aula02Sergio;
+using System;
 using System.Threading.Tasks.Dataflow;
 
 namespace Aula02
@@ -13,13 +14,15 @@ namespace Aula02
             Console.WriteLine("Nome:" + produto1.nome);
             Console.WriteLine("Quantidade:" + produto1.quantidade);
             Console.WriteLine("Preço:" + produto1.preco);
+            Console.WriteLine("Validade:" + produto1.validade);
 
-            Produto produto2 = new Produto(1, "Coca-Cola", 15, 8.99);
+            Produto produto2 = new Produto(1, "Coca-Cola", 15, 8.99, DateTime.Now);
 
             Console.WriteLine("\n\nCódigo:" + produto2.codigo);
             Console.WriteLine("Nome:" + produto2.nome);
             Console.WriteLine("Quantidade:" + produto2.quantidade);
             Console.WriteLine("Preço:" + produto2.preco);
+            Console.WriteLine("Validade:" + produto2.validade);
 
             Produto produto3 = new Produto("Fanta", 6);
 
@@ -27,6 +30,16 @@ namespace Aula02
             Console.WriteLine("Nome:" + produto3.nome);
             Console.WriteLine("Quantidade:" + produto3.quantidade);
             Console.WriteLine("Preço:" + produto3.preco);
+            Console.WriteLine("Validade:" + produto3.validade);
+
+            Pessoa pessoa1 = new Pessoa();
+            pessoa1.Codigo = 1;
+            pessoa1.Nome = "Camila";
+            pessoa1.Altura = 1.56;
+           
+            Console.WriteLine("\n\nCódigo:" + pessoa1.Codigo);
+            Console.WriteLine("Nome:" + pessoa1.Nome);
+            Console.WriteLine("Altura:" + pessoa1.Altura);
 
         }
     }
