cf8e4eed511e3c7413607c142be90fcc30f4e661
TamaraAlessandra <tamara@gemmap.com.br>
2020-09-24 12:29:12 -0300

    Novo projeto Aula02, classe produto

---
+++ Aula02Sergio/Aula02Sergio/Produto.cs
@@ -0,0 +1,36 @@
+﻿namespace Aula02
+{
+    class Produto
+    {
+        public int codigo;
+        public string nome;
+        public int quantidade;
+        public double preco;
+
+        public Produto() //construtor alterado
+        {
+            codigo = -1;
+            nome = "A Definir";
+            quantidade = -1;
+            preco = -1.0;
+        }
+
+        public Produto(int codigo, string nome, int quantidade, double preco) //construtor com parâmetro
+        {
+            this.codigo = codigo; //1°codigo = da classe    2°codigo = parâmetro
+            this.nome = nome;
+            this.quantidade = quantidade;
+            this.preco = preco;
+        }
+
+        public Produto(string nome, int quantidade)
+        {
+            this.nome = nome;
+            this.quantidade = quantidade;
+
+        }
+
+    }
+
+
+}
---
+++ Aula02Sergio/Aula02Sergio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace Aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto1 = new Produto();
+
+            Console.WriteLine("\n\nCódigo:" + produto1.codigo);
+            Console.WriteLine("Nome:" + produto1.nome);
+            Console.WriteLine("Quantidade:" + produto1.quantidade);
+            Console.WriteLine("Preço:" + produto1.preco);
+
+            Produto produto2 = new Produto(1, "Coca-Cola", 15, 8.99);
+
+            Console.WriteLine("\n\nCódigo:" + produto2.codigo);
+            Console.WriteLine("Nome:" + produto2.nome);
+            Console.WriteLine("Quantidade:" + produto2.quantidade);
+            Console.WriteLine("Preço:" + produto2.preco);
+
+            Produto produto3 = new Produto("Fanta", 6);
+
+            Console.WriteLine("\n\nCódigo:" + produto3.codigo);
+            Console.WriteLine("Nome:" + produto3.nome);
+            Console.WriteLine("Quantidade:" + produto3.quantidade);
+            Console.WriteLine("Preço:" + produto3.preco);
+
+        }
+    }
+}
