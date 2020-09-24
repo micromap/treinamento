e9c9e050fcfb984f6014a30010551daec286eafe
TamaraAlessandra <tamara@gemmap.com.br>
2020-09-24 17:10:42 -0300

    Classe Produto

--- Aula02Sergio/Aula02Sergio/Produto.cs
+++ Aula02Sergio/Aula02Sergio/Produto.cs
@@ -1,4 +1,6 @@
-﻿namespace Aula02
+﻿using System;
+
+namespace Aula02
 {
     class Produto
     {
@@ -6,6 +8,7 @@
         public string nome;
         public int quantidade;
         public double preco;
+        public DateTime validade;
 
         public Produto() //construtor alterado
         {
@@ -15,12 +18,13 @@
             preco = -1.0;
         }
 
-        public Produto(int codigo, string nome, int quantidade, double preco) //construtor com parâmetro
+        public Produto(int codigo, string nome, int quantidade, double preco, DateTime validade) //construtor com parâmetro
         {
             this.codigo = codigo; //1°codigo = da classe    2°codigo = parâmetro
             this.nome = nome;
             this.quantidade = quantidade;
             this.preco = preco;
+            this.validade = validade;
         }
 
         public Produto(string nome, int quantidade)
