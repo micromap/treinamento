d307d756dd3339eda988a520c1c88e34f86da8d5
rsecco <rsecco@PC022>
2020-09-25 17:08:27 -0300

    4 maneiras de concatenar strings

--- Exercicios/Exercicios.Domain/Cachorro.cs
+++ Exercicios/Exercicios.Domain/Cachorro.cs
@@ -12,6 +12,10 @@ namespace Exercicios.Domain
         public string QuantoDevoComer(int peso, string nome)
         {
             return nome + " pesando " + peso + "Kg deve comer " + (peso * 1000) * (0.05) + "g diariamente.";
+
+            //return string.Join("", nome, " pesando ", peso, "Kg deve comer ", (peso * 1000) * (0.05), "g diariamente.");
+            //return $"{nome} pesando {peso}Kg deve comer {(peso * 1000) * (0.05)}g diariamente.";
+            //return string.Format("{0}{1}{2}{3}{4}{5}", nome," pesando ", peso,"Kg deve comer ", (peso * 1000) * (0.05), "g diariamente.");
         }
     }
 }
--- Exercicios/Exercicios.Tests/CachorroTest.cs
+++ Exercicios/Exercicios.Tests/CachorroTest.cs
@@ -50,18 +50,18 @@ namespace Exercicios.Tests
 
 
             
-                        /*if (quantoDevoComer.Substring(0, 4) == "Leia")
-                        {
-                            Assert.AreEqual("Leia pesando 1Kg deve comer 50g diariamente.", quantoDevoComer);
-                        }
-                        else if (quantoDevoComer.Substring(0, 7) == "Tequila")
-                        {
-                            Assert.AreEqual("Tequila pesando 30Kg deve comer 1500g diariamente.", quantoDevoComer);
-                        }
-                        else
-                        {
-                            Assert.AreEqual("Yuri pesando 15Kg deve comer 750g diariamente.", quantoDevoComer);
-                        }*/
+            /*if (quantoDevoComer.Substring(0, 4) == "Leia")
+            {
+                Assert.AreEqual("Leia pesando 1Kg deve comer 50g diariamente.", quantoDevoComer);
+            }
+            else if (quantoDevoComer.Substring(0, 7) == "Tequila")
+            {
+                Assert.AreEqual("Tequila pesando 30Kg deve comer 1500g diariamente.", quantoDevoComer);
+            }
+            else
+            {
+                Assert.AreEqual("Yuri pesando 15Kg deve comer 750g diariamente.", quantoDevoComer);
+            }*/
         }
     }
 }
