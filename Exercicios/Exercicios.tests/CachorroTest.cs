700c320cda7bb95164f34519a6662d123053589e
marco1505 <marquinho@gemmap.com.br>
2020-10-01 16:16:56 -0300

    de 20kg para 30kg

--- Exercicios/Exercicios.tests/CachorroTest.cs
+++ Exercicios/Exercicios.tests/CachorroTest.cs
@@ -20,6 +20,7 @@ namespace Exercicios.tests
 
         /*
         Considerando que e Léia pesa 20kg,
+        Considerando que e Léia pesa 30kg,
         e come 5% do seu peso de ração,
         implemente o método "QuandoDevoComer"
         para passar nesse teste
