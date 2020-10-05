b4e34d6dde317a27e3ad5f913815f87f19caa35d
TamaraAlessandra <tamara@gemmap.com.br>
2020-10-05 17:28:06 -0300

    Test conversão e tipo de  valor

--- Exercicios/Exercicios.Domain/HelloWorld.cs
+++ Exercicios/Exercicios.Domain/HelloWorld.cs
@@ -1,10 +1,22 @@
-﻿namespace Exercicios.Domain
+﻿using System;
+
+namespace Exercicios.Domain
 {
     public static class HelloWorld
     {
         public static string SayHello()
         {
             return "Hello World!";
+        }
+
+        public static void PassaValor(int valor)
+        {
+            valor = 999;
+        }
+
+        public static void PassaReferencia(Cachorro cachorro)
+        {
+            cachorro.SetNome("Léia");
         }
     }
 }
--- Exercicios/Exercicios.Tests/HelloWorldTest.cs
+++ Exercicios/Exercicios.Tests/HelloWorldTest.cs
@@ -16,5 +16,41 @@ namespace Exercicios.Tests
 
             Console.WriteLine(mensagem);
        }
+        [TestMethod]
+        public void Conversao_Implicita_Test()
+        {
+            int inteiro = 10;
+            double valor = inteiro;   //converte inteiro para double
+
+            Console.WriteLine(valor);
+            Assert.AreEqual(inteiro, valor);
+        }
+        [TestMethod]
+        public void Conversao_Explicita_Test()
+        {
+            double valor = 1.2;
+            int inteiro = (int)valor;    //cast explicit
+
+            Console.WriteLine(inteiro);
+            Assert.AreNotEqual(valor, inteiro);  //primeiro resultado esperado, depois o que é pra comparar
+        }
+        [TestMethod]
+        public void Tipo_de_Valor_Test()
+        {
+            int valor = 10;
+            HelloWorld.PassaValor(valor);
+
+            Console.WriteLine(valor);
+            Assert.AreEqual(10, valor);
+        }
+        [TestMethod]
+        public void Tipo_de_Referencia_Test()
+        {
+            Cachorro cachorro = new Cachorro();
+            cachorro.SetNome("Léia");
+
+            HelloWorld.PassaReferencia(cachorro);
+        }
     }
+
 }
