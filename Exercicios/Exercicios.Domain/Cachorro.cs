703ea2cdb74a723771707fa061eecc03103ed226
TamaraAlessandra <tamara@gemmap.com.br>
2020-10-05 12:31:33 -0300

    Criando região dos atributos

--- Exercicios/Exercicios.Domain/Cachorro.cs
+++ Exercicios/Exercicios.Domain/Cachorro.cs
@@ -4,12 +4,91 @@ namespace Exercicios.Domain
 {
     public class Cachorro
     {
+        #region Nome
         private string _nome;
+
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+        #endregion
+
+        #region Sexo
         private string _sexo;
+        public void SetSexo(string sexo)
+        {
+            _sexo = sexo;
+        }
+
+        public string GetSexo()
+        {
+            return _sexo;
+        }
+        #endregion
+
+        #region Raça
         private string _raca;
+        public void SetRaca(string raca)
+        {
+            _raca = raca;
+        }
+
+        public string GetRaca()
+        {
+            return _raca;
+        }
+        #endregion
+
+        #region Porte
         private string _porte;
+        public void SetPorte(string porte)
+        {
+            _porte = porte;
+        }
+
+        public string GetPorte()
+        {
+            return _porte;
+        }
+        #endregion
+
+        #region Idade
         private int _idade;
+        public void SetIdade(int idade)
+        {
+            _idade = idade;
+        }
+
+        public int GetIdade()
+        {
+            return _idade;
+        }
+        #endregion
+
+        #region Peso
         private double _pesoKg;
+        public void SetPeso(double pesoKg)
+        {
+            if (pesoKg < 0)
+            {
+                _pesoKg = 0;
+            }
+            else
+            {
+                _pesoKg = pesoKg;
+            }
+        }
+        public double GetPeso()
+        {
+            return _pesoKg;
+
+        }
+        #endregion
 
         public string Latir()
         {
@@ -21,68 +100,7 @@ namespace Exercicios.Domain
             double qtdeRacaoGramas = pesoKg * 50;
 
             return $"Como tenho {pesoKg}kg, devo comer {qtdeRacaoGramas}g por dia.";
-        }
-
-        public void SetNome(string nome)
-        {
-            _nome = nome;
-        }
-
-        public string GetNome()
-        {
-            return _nome;
-        }
-
-        public void SetSexo(string sexo)
-        {
-            _sexo = sexo;
-        }
-
-        public string GetSexo()
-        {
-            return _sexo;
-        }
-
-        public void SetRaca(string raca)
-        {
-            _raca = raca;
-        }
-
-        public string GetRaca()
-        {
-            return _raca;
-        }
-
-        public void SetPorte(string porte)
-        {
-            _porte = porte;
-        }
-
-        public string GetPorte()
-        {
-            return _porte;
-        }
-
-        public void SetIdade(int idade)
-        {
-            _idade = idade;
-        }
-
-        public int GetIdade()
-        {
-            return _idade;
-        }
-
-        public void SetPeso(double pesoKg)
-        {
-            _pesoKg = pesoKg;
-        }
-
-        public double GetPeso()
-        {
-            return _pesoKg;
-
-        }
+        }        
     }
 
 }
