977a99190e5045b92211b3030a2d39fd5495ce77
TamaraAlessandra <tamara@gemmap.com.br>
2020-10-08 11:42:28 -0300

    Create PessoaFisica.cs

---
+++ Aula03Sergio/Aula03Sergio/PessoaFisica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula03Sergio
+{
+    class PessoaFisica : Pessoa
+    {
+        public string Nome { get; set; }
+
+        public string CPF { get; set; }
+
+        public string RG { get; set; }
+
+    }
+}
