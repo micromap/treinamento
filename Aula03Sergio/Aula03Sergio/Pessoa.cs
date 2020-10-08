135c7c0f4dbb7bfc210814bbbf273fe99416e61c
TamaraAlessandra <tamara@gemmap.com.br>
2020-10-08 11:42:32 -0300

    Create Pessoa.cs

---
+++ Aula03Sergio/Aula03Sergio/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace Aula03Sergio
+{
+    class Pessoa
+    {
+        public int Codigo { get; set; }
+
+        public DateTime Criacao { get; set; }
+
+        public string Email { get; set; }
+
+        public string Celular { get; set; }
+    }
+}
