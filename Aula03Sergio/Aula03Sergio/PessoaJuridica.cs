14aef8eb21b0c09af9b4ecf3a5a461ace90486a2
TamaraAlessandra <tamara@gemmap.com.br>
2020-10-08 11:42:12 -0300

    Create PessoaJuridica.cs

---
+++ Aula03Sergio/Aula03Sergio/PessoaJuridica.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Aula03Sergio
+{
+    class PessoaJuridica : Pessoa
+    {
+        public string CNPJ { get; set; }
+
+        public string InscricaoEstadual { get; set; }
+
+        public string RazaoSocial { get; set; }
+
+        public string NomeFantasia { get; set; }
+    }
+}
