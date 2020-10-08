a91753a043e025ce57e19bc96c9352d8c92389f4
TamaraAlessandra <tamara@gemmap.com.br>
2020-10-08 11:42:37 -0300

    Create Funcionario.cs

---
+++ Aula03Sergio/Aula03Sergio/Funcionario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula03Sergio
+{
+    class Funcionario : PessoaFisica
+    {
+        public string CTPS { get; set; }
+
+        public string PIS { get; set; }
+
+    }
+}
