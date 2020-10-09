6e6f1fa8e54ce7259a84028e59f034ba131c18dc
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-10-09 17:10:41 -0300

    016 - Tipos Explícitos, Implícitos e Nuláveis

--- Exercicios/Exercicios.Tests/HelloWorldTest.cs
+++ Exercicios/Exercicios.Tests/HelloWorldTest.cs
@@ -97,6 +97,81 @@ namespace Exercicios.Tests
 
             Assert.AreEqual(cachorro1, cachorro2);
         }
+
+        [TestMethod]
+        public void Tipos_Explicitos_Test()
+        {
+            string nome = "Leia";
+            int idade = 1;
+            double peso = 1.3;
+            Cachorro cachorro = new Cachorro();
+
+            Assert.AreEqual(typeof(string), nome.GetType());
+            Assert.AreEqual(typeof(int), idade.GetType());
+            Assert.AreEqual(typeof(double), peso.GetType());
+            Assert.AreEqual(typeof(Cachorro), cachorro.GetType());
+        }
+
+        [TestMethod]
+        public void Tipos_Implicitos_Test()
+        {
+            var nome = "Leia";
+            var idade = 1;
+            var peso = 1.3;
+            var cachorro = new Cachorro();
+
+            Assert.AreEqual(typeof(string), nome.GetType());
+            Assert.AreEqual(typeof(int), idade.GetType());
+            Assert.AreEqual(typeof(double), peso.GetType());
+            Assert.AreEqual(typeof(Cachorro), cachorro.GetType());
+        }
+
+        [TestMethod]
+        public void Declarar_Sem_Inicializar_Test()
+        {
+            Cachorro cachorro;
+            cachorro = new Cachorro();
+
+            Assert.AreEqual(typeof(Cachorro), cachorro.GetType());
+
+            // Não podemos declarar as variáveis implicitas sem atribuir valor
+            //var nome;
+            //nome = "Leia";
+            //Assert.AreEqual(typeof(string), nome.GetType());
+
+            string nome;
+            nome = "Leia";
+
+            Assert.AreEqual(typeof(string), nome.GetType());
+        }
+
+        [TestMethod]
+        public void Tipos_Que_Aceitam_Null_Test()
+        {
+            string nome = null;
+            Cachorro cachorro = null;
+
+            Assert.AreEqual(null, nome);
+            
+            Assert.AreEqual(null, cachorro);
+
+            //int idade = null;
+            //double peso = null;
+            //Assert.AreEqual(null, idade);
+            //Assert.AreEqual(null, peso);
+        }
+
+        [TestMethod]
+        public void Tipos_Nulaveis_Test()
+        {
+            int? idade = null;
+            double? peso = null;
+            bool? vacinado = null;
+
+            Assert.AreEqual(null, idade);
+            Assert.AreEqual(null, peso);
+            Assert.AreEqual(null, vacinado);
+        }
     }
 }
 
