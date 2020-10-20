3a272282810b5600eb9ee42358aec42bace0ad14
lali@gemmap.com.br <lali@gemmap.com.br>
2020-10-20 17:28:54 -0300

    implementação de testes unitários

---
+++ Exercicios/Exercicios.Tests/CachorroTest.cs
@@ -0,0 +1,41 @@
+﻿using Exercicios.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Exercicios.Tests
+{
+    [TestClass]
+    public class CachorroTest
+    {
+        [TestMethod]
+        public void Cachorro_Latir_Test()
+        {
+            Cachorro leia = new Cachorro();
+            string latido = leia.Latir();
+
+            Console.WriteLine(latido);
+
+            Assert.AreEqual("Au! Au!", latido);
+                  
+        }
+
+        // Considerando que a Leia pesa 1Kg
+        // e come 5% do seu peso de ração,
+        // Implemente o método "QuantoDevoComer"
+        // para passar nesse teste
+
+        [TestMethod]
+        public void Leia_QuandoDevoComer_Test()
+        {
+            Cachorro leia = new Cachorro();
+            string QuantoDevoComer = leia.QuantoDevoComer(1);
+
+            Console.WriteLine(QuantoDevoComer);
+
+            Assert.AreEqual("Como tenho 1Kg, devo comer 50g por dia", QuantoDevoComer);
+        }
+
+        // Criar os métodos de teste, para a Tequila e para o Yuri
+
+    }
+}
