2b1ab1c1e262da4d55e06aff2d98abe990b7fb12
gizelev <gizele@gemmap.com.br>
2020-10-21 15:47:58 -0300

    aula14
    aula 014

--- Exercicios/Exercicios.domain/cachorro.cs
+++ Exercicios/Exercicios.domain/cachorro.cs
@@ -3,7 +3,15 @@
 namespace Exercicios.domain
 {
    public class Cachorro
-    { 
+    {
+        private string _nome;
+        private string _sexo;
+        private string _raca;
+        private string _porte;
+        private int    _idade;
+        private double _pesokg;
+
+
         public string Latir()
         {
             return "Au Au!";         
@@ -13,6 +21,38 @@ namespace Exercicios.domain
         {
             double quantidade = peso * 1000 * 0.05;
             return "Devo Comer : " + quantidade;
+        }
+
+
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetRaca(string raca)
+        {
+            _raca = raca;
+        }
+
+        public string GetRaca()
+        {
+            return _raca;
+        }
+
+        public void SetPorte(string porte)
+        {
+            _porte = porte;
+        }
+
+        public string GetPorte()
+        {
+            return _raca;
         }
     }
 }
--- Exercicios/Exercicios.test/cachorrotest.cs
+++ Exercicios/Exercicios.test/cachorrotest.cs
@@ -5,22 +5,34 @@ using System;
 namespace Exercicios.test
 {
   
-    [TestClass]
+   [TestClass]
         public class Cachorrotest
     {
-         [TestMethod]
-        public void cachorro_latir_test()
+         private object leia;
+
+        public Cachorrotest(object leia)
         {
-            Cachorro leia;
-            string latido = leia.Latir(); 
+            this.leia = leia;
+        }
+
+        [TestMethod]
+       public void cachorro_latir_test()
+        {
+            string latido = Latir();
 
 
-            Console.WriteLine(latido);
+           Console.WriteLine(latido);
             
-            Assert.AreEqual("Au, AU!", latido);            
-
+           Assert.AreEqual("Au, AU!", latido);       
+            
          }
-        // Considerando que a Léia pesa 1kg,
+
+        private string Latir()
+        {
+            throw new NotImplementedException();
+        }
+
+         //Considerando que a Léia pesa 1kg,
         // e come 5% do seu peso de ração,
         // implemente o método "quanto devo comer"
         // para passar neste teste
@@ -34,7 +46,41 @@ namespace Exercicios.test
             Console.WriteLine(quantoDevoComer);
 
             Assert.AreEqual("Devo Comer : 50", quantoDevoComer);
+           }
+
+        [TestMethod]
+
+        public void Cachorro_Set_Get_Nome_Test()
+        {
+            Cachorro leia = new Cachorro();
+            leia.SetNome("Leia");
+            string nome = leia.GetNome();
+
+            Console.WriteLine(nome);
+            Assert.AreEqual("Leia", nome);
         }
 
-     }
-}
+        [TestMethod]
+        public void Cachorro_Set_Get_Raca_test()
+        {
+            Cachorro leia = new Cachorro();
+            leia.SetRaca("Femea");
+            string raca = leia.GetRaca();
+
+            Console.WriteLine(raca);
+            Assert.AreEqual("Femea",raca);
+          }
+
+         [TestMethod]
+            public void Cahorro_Set_Get_Porte_test()
+        {
+            Cachorro leia = new Cachorro();
+            leia.SetPorte("Pequeno");
+            string porte = leia.GetPorte();
+
+            Console.WriteLine(porte);
+            Assert.AreEqual("Pequeno", porte);
+        }
+    }
+
+  }
