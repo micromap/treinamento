5eaaf6690135c7a6846ef8172313966f3ec38811
lali@gemmap.com.br <lali@gemmap.com.br>
2020-10-27 09:57:39 -0300

    implementando teste de tipos de valor e tipos de referencia

--- Exercicios/Exercicios.Tests/HelloWorldTest.cs
+++ Exercicios/Exercicios.Tests/HelloWorldTest.cs
@@ -16,5 +16,80 @@ namespace Exercicios.Tests
 
             Console.WriteLine(mensagem);
         }
+
+        [TestMethod]
+        public void Tipos_Primitivos_e_Complexos_Test()
+        {
+            // Tipos Primitivos
+            string mensagem = "Hello World!";
+            double peso = 1.2;
+            int idade = 4;
+
+            Console.WriteLine(mensagem);
+            Console.WriteLine(peso);
+            Console.WriteLine(idade);
+
+            // Tipos Complexos
+            Cachorro leia = new Cachorro();
+            Console.WriteLine(leia);
+        }
+
+        [TestMethod]
+        public void Conversao_Implicita_Test()
+        {
+            int inteiro = 10;
+            double valor = inteiro;
+
+            Console.WriteLine(valor);
+            Assert.AreEqual(inteiro, valor);
+        }
+
+        [TestMethod]
+        public void Conversao_Explicita_Test()
+        {
+            double valor = 1.23;
+            int inteiro = (int)valor;
+            
+
+            Console.WriteLine(inteiro);
+            Assert.AreNotEqual(valor, inteiro);
+        }
+
+        [TestMethod]
+        public void Tipo_de_Valor_Test()
+        {
+            int valor = 10;
+            HelloWorld.PassaValor(valor);
+            int inteiro = (int)valor;
+
+
+            Console.WriteLine(valor);
+            Assert.AreEqual(10, valor);
+        }
+
+        [TestMethod]
+        public void Tipo_de_Referencia_Test()
+        {
+            Cachorro cachorro = new Cachorro();
+            cachorro.SetNome("Léia");
+
+            HelloWorld.PassaReferencia(cachorro);
+
+            string nomeCachorro = cachorro.GetNome();
+            
+            Console.WriteLine(nomeCachorro);
+            Assert.AreEqual("Tequila", nomeCachorro);
+        }
+        [TestMethod]
+        public void Igualdade_entre_Tipos_de_Valor_Test()
+        {
+            int valor1 = 10;
+            int valor2 = 10;
+
+            Assert.AreEqual(valor1, valor2);
+        }
+
+            
+
     }
 }
