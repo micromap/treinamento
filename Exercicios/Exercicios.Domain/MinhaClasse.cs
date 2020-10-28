c4e798633cdff46e8d3ea5e2b6d4ca47ef6f6380
lali@gemmap.com.br <lali@gemmap.com.br>
2020-10-28 14:37:49 -0300

    Comentando a classe MinhaClasse

--- Exercicios/Exercicios.Domain/MinhaClasse.cs
+++ Exercicios/Exercicios.Domain/MinhaClasse.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Exercicios.Domain
 {
+    // public class     => Acesso de dentro e fora do assembly
+    // internal class   => Acesso apenas de dentro do assembly
+    // class            => O mesmo que internal class
     class MinhaClasse
     {
     }
