f920306becb0f41677e88a4cb09100d88bd275f9
ederlap-gemmap <ederlap@gemmap.com.br>
2020-11-10 16:02:00 -0300

    update code

--- exercicios/Exercicios.Domain/Cachorro.cs
+++ exercicios/Exercicios.Domain/Cachorro.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Exercicios.Domain
 {
@@ -8,18 +10,44 @@ namespace Exercicios.Domain
         public string Sexo { get; set; }
         public string  Raca {get; set;}
         public string Porte { get; set; }
-        public int Idata { get; set; }
+        public int Idata { get; set;  }
         public double Peso { get; set; }
+        public bool Vacinado { get; set; }
 
-        public string Latir()
+        /*
+         public string Latir(string latir)
+         { 
+             return "Au! Au!"; 
+         }
+        */ 
+
+        public string Latir(short quantidadeDeLatidos)
         {
-            return "Au! Au!";
+            string latidos = "";
+            for(short i =1; 1 <= quantidadeDeLatidos; i++)
+            {
+                latidos = latidos + "Au!";
+            }
+            return latidos;
         }
-        
+
+
         public string QuantoDeveComer(int pesoKg)
         // metrodo para calcular 5% do peso (Kg) do cachorro em gramas de Ração;
         {
             return $"Como tenho {pesoKg}kg, devo comer {pesoKg * 50.0}g por dia";           
-        }     
+        }
+
+        public List<string> Valida()
+        {
+            List<string> message = new List<string>();
+
+            if (string.IsNullOrWhiteSpace(Nome))
+            {
+                message.Add("Nome do cachorro é obrigatório!");
+            }
+
+            return message.Count == 0 ? message : null;
+        }
     }
 }
--- exercicios/Exercicios.Test/CachorroTest.cs
+++ exercicios/Exercicios.Test/CachorroTest.cs
@@ -9,14 +9,12 @@ namespace Exercicios.Test
 
     {
         [TestMethod]
-        public void CachorroLatirTest()
+        public void Cachorro_Latir_Test()
         {
-            Cachorro leia = new Cachorro();
-            string latido = leia.Latir();
-            Assert.AreEqual("Au! Au!", latido);
+            var leia = new Cachorro();
+            string latido = leia.Latir(3);
+            Assert.AreEqual("Au! Au! Au! ", latido);
             Console.WriteLine(latido);
-
-
         }
 
         /* Considerando quea Léia pesa 1KG,
@@ -26,48 +24,66 @@ namespace Exercicios.Test
          */
 
         [TestMethod]
-        public void LeiaQuantoDevoComerTest()
+        public void Leia_Quanto_Devo_Comer_Test()
         {
-            Cachorro leia = new Cachorro();
+            var leia = new Cachorro();
             string quantoDevoComer = leia.QuantoDeveComer(1);
             Assert.AreEqual("Como tenho 1kg, devo comer 50g por dia", quantoDevoComer);
             Console.WriteLine(quantoDevoComer);
         }
         
         [TestMethod]
-        public void TequilaQuantoDevoComerTest()
+        public void Tequila_Quanto_Devo_Comer_Test()
         {
-            Cachorro tequila = new Cachorro();
+            var tequila = new Cachorro();
             string quantoDevoComer = tequila.QuantoDeveComer(30);
             Assert.AreEqual("Como tenho 30kg, devo comer 1500g por dia", quantoDevoComer);
             Console.WriteLine(quantoDevoComer);
         }
 
         [TestMethod]
-        public void YuriQuantoDevoComerTest()
+        public void Yuri_Quanto_Devo_Comer_Test()
         {
-            Cachorro yuri = new Cachorro();
+            var yuri = new Cachorro();
             string quantoDevoComer = yuri.QuantoDeveComer(15);
             Assert.AreEqual("Como tenho 15kg, devo comer 750g por dia", quantoDevoComer);
             Console.WriteLine(quantoDevoComer);
         }
 
         [TestMethod]
-        public void YuriGetSetNomeTeste()
+        public void Yuri_Get_Set_Nome_Teste()
         {
-            Cachorro yuri = new Cachorro();
+            var yuri = new Cachorro();
             yuri.Nome = "Yuri";
             string nome = yuri.Nome;
             Assert.AreEqual("Yuri", nome);
         }
 
         [TestMethod]
-        public void YuriGetSetSexoTeste()
+        public void Yuri_Get_Set_Sexo_Teste()
         {
-            Cachorro yuri = new Cachorro();
+            var yuri = new Cachorro();
             yuri.Sexo = "Macho";
             string nome = yuri.Sexo;
             Assert.AreEqual("Macho", nome);
         }
+        
+        [TestMethod]
+        public void Cachorro_Nome_Obrigatorio_Teste()
+        {
+            var cachorro = new Cachorro();
+            var message = cachorro.Valida();
+            Assert.AreNotEqual("Nome do cachorro é obrigatório!", message[0]);
+            Console.WriteLine(message[0]);
+        }
+
+        [TestMethod]
+        public void Cachorro_Macho_Femea_Teste()
+        {
+            var cachorro = new Cachorro();
+            Console.WriteLine("");
+            
+        }
+      
     }
 }
