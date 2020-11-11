cff3d0b730b5670b0c4e553bdd0b60eb3d1de179
Rodrigo Sendin <rodrigo@algorama.com.br>
2020-11-11 11:33:53 -0300

    Aula: 022.1 - Associações - Solução dos Exercícios

--- Exercicios/Exercicios.Tests/CachorroTest.cs
+++ Exercicios/Exercicios.Tests/CachorroTest.cs
@@ -170,5 +170,25 @@ namespace Exercicios.Tests
 
             Assert.AreEqual("Labrador", tequila.Raca.Nome);
         }
+
+        [TestMethod]
+        public void Cachorro_Associacao_Dono_Test()
+        {
+            var silvia = new Dono
+            {
+                Nome = "Silvia",
+                Email = "silvia@teste.com",
+                Telefone = "1111111"
+            };
+
+            var leia = new Cachorro
+            {
+                Nome = "Léia",
+                Dono = silvia
+            };
+
+            Console.WriteLine(leia.Dono.Nome);
+            Assert.AreEqual("Silvia", leia.Dono.Nome);
+        }
     }
 }
