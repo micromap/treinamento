323b0219f9160cc5af7f4829ac08aa939e783031
Fernando Depizol <42611622+fernandodepizol@users.noreply.github.com>
2020-11-13 10:38:54 -0300

    Classe Cachorro e exercícios

---
+++ Treino/Exercicios/Exercicios.Domain/Cachorro.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace Exercicios.Domain
+{
+    public class Cachorro
+    {
+        public Double Peso;
+        public string nome; 
+
+
+
+
+        public string Latir()
+        {
+            return "au au au au!";
+        }
+
+
+
+        //Método para calcular 5% do peso do cachorro em ração, é o que ele deve comer.
+        public string QuantoDevoComer(Double pesoCachorro)
+        {
+            this.Peso = pesoCachorro;
+            Double PesoAComer = pesoCachorro * 0.05;
+            
+            //cifrão na frente do texto e concatena usando chaves (Interpolação de string)
+            return $"Como tenho {pesoCachorro}Kg, devo comer {PesoAComer}g por dia";
+        }
+    }
+}
---
+++ Treino/Exercicios/Exercicios.Testes/CachorroTest.cs
@@ -0,0 +1,57 @@
+﻿
+using Exercicios.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Exercicios.Tests
+{
+    [TestClass]
+    public class CachorroTest
+    {
+
+        [TestMethod]
+        public void Cachorro_Latir_Test()
+        {
+            Cachorro leia = new Cachorro();
+            string latido = leia.Latir();
+
+            Console.WriteLine(latido);
+
+            Assert.AreEqual("au au au au!", latido);
+
+        } 
+
+
+        [TestMethod]
+        public void Leia_QtoDevoComer_Test()
+        {
+            Cachorro leia = new Cachorro();
+            leia.Peso = 1;
+            string QuantoDevoComer = leia.QuantoDevoComer(leia.Peso);
+            Console.WriteLine(QuantoDevoComer);
+            //usando interpolação de string
+            Assert.AreEqual($"Como tenho {leia.Peso}Kg, devo comer {leia.Peso * 0.05}g por dia", QuantoDevoComer);
+        }
+
+        [TestMethod]
+        public void Tequila_QtoDevoComer_Test()
+        {
+            Cachorro tequila = new Cachorro();
+            tequila.Peso = 30;
+            string QuantoDevoComer = tequila.QuantoDevoComer(tequila.Peso);
+            Console.WriteLine(QuantoDevoComer);
+            Assert.AreEqual("Como tenho " + tequila.Peso + "Kg, devo comer " + (tequila.Peso * 0.05) + "g por dia", QuantoDevoComer);
+        }
+       
+        
+        [TestMethod]
+        public void Yury_QtoDevoComer_Test()
+        {
+            Cachorro yury = new Cachorro();
+            yury.Peso = 15;
+            string QuantoDevoComer = yury.QuantoDevoComer(yury.Peso);
+            Console.WriteLine(QuantoDevoComer);
+            Assert.AreEqual("Como tenho "+yury.Peso+"Kg, devo comer "+(yury.Peso*0.05)+"g por dia",QuantoDevoComer);
+        }
+    }
+}
