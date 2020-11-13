c6234e551ec5a56981993366fb48d5b4a8f57ace
Fernando Depizol <42611622+fernandodepizol@users.noreply.github.com>
2020-11-13 11:48:32 -0300

    Aula14 - Get Set atributos

--- Treino/Exercicios/Exercicios.Domain/Cachorro.cs
+++ Treino/Exercicios/Exercicios.Domain/Cachorro.cs
@@ -5,8 +5,13 @@ namespace Exercicios.Domain
 {
     public class Cachorro
     {
-        public Double Peso;
-        public string nome; 
+        private string _nome;
+        private string _sexo;
+        private string _raca;
+        private string _porte;
+        private int    _idade;
+        private double _peso;
+
 
 
 
@@ -21,11 +26,71 @@ namespace Exercicios.Domain
         //Método para calcular 5% do peso do cachorro em ração, é o que ele deve comer.
         public string QuantoDevoComer(Double pesoCachorro)
         {
-            this.Peso = pesoCachorro;
+            this._peso = pesoCachorro;
             Double PesoAComer = pesoCachorro * 0.05;
             
             //cifrão na frente do texto e concatena usando chaves (Interpolação de string)
             return $"Como tenho {pesoCachorro}Kg, devo comer {PesoAComer}g por dia";
+        }
+
+        public double GetPeso()
+        {
+            return _peso;
+        }
+
+        public void SetPeso(int peso)
+        {
+            this._peso = peso;
+        }
+
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetSexo(string sexo)
+        {
+            this._sexo = sexo;
+        }
+
+        public string GetSexo()
+        {
+            return this._sexo;
+        }
+
+        public void SetRaca(string raca)
+        {
+            this._raca = raca;
+        }
+
+        public string GetRaca()
+        {
+            return this._raca;
+        }
+
+        public void SetPorte(string porte)
+        {
+            this._porte = porte;
+        }
+
+        public string GetPorte()
+        {
+            return this._porte;
+        }
+
+        public void SetIdade(int idade)
+        {
+            this._idade = idade;
+        }
+
+        public int GetIdade()
+        {
+            return this._idade;
         }
     }
 }
--- Treino/Exercicios/Exercicios.Testes/CachorroTest.cs
+++ Treino/Exercicios/Exercicios.Testes/CachorroTest.cs
@@ -26,21 +26,21 @@ namespace Exercicios.Tests
         public void Leia_QtoDevoComer_Test()
         {
             Cachorro leia = new Cachorro();
-            leia.Peso = 1;
-            string QuantoDevoComer = leia.QuantoDevoComer(leia.Peso);
+            leia.SetPeso(1);
+            string QuantoDevoComer = leia.QuantoDevoComer(leia.GetPeso());
             Console.WriteLine(QuantoDevoComer);
             //usando interpolação de string
-            Assert.AreEqual($"Como tenho {leia.Peso}Kg, devo comer {leia.Peso * 0.05}g por dia", QuantoDevoComer);
+            Assert.AreEqual($"Como tenho {leia.GetPeso()}Kg, devo comer {leia.GetPeso() * 0.05}g por dia", QuantoDevoComer);
         }
 
         [TestMethod]
         public void Tequila_QtoDevoComer_Test()
         {
             Cachorro tequila = new Cachorro();
-            tequila.Peso = 30;
-            string QuantoDevoComer = tequila.QuantoDevoComer(tequila.Peso);
+            tequila.SetPeso(30);
+            string QuantoDevoComer = tequila.QuantoDevoComer(tequila.GetPeso());
             Console.WriteLine(QuantoDevoComer);
-            Assert.AreEqual("Como tenho " + tequila.Peso + "Kg, devo comer " + (tequila.Peso * 0.05) + "g por dia", QuantoDevoComer);
+            Assert.AreEqual("Como tenho " + tequila.GetPeso() + "Kg, devo comer " + (tequila.GetPeso() * 0.05) + "g por dia", QuantoDevoComer);
         }
        
         
@@ -48,10 +48,83 @@ namespace Exercicios.Tests
         public void Yury_QtoDevoComer_Test()
         {
             Cachorro yury = new Cachorro();
-            yury.Peso = 15;
-            string QuantoDevoComer = yury.QuantoDevoComer(yury.Peso);
+            yury.SetPeso(15);
+            string QuantoDevoComer = yury.QuantoDevoComer(yury.GetPeso());
             Console.WriteLine(QuantoDevoComer);
-            Assert.AreEqual("Como tenho "+yury.Peso+"Kg, devo comer "+(yury.Peso*0.05)+"g por dia",QuantoDevoComer);
+            Assert.AreEqual("Como tenho "+yury.GetPeso()+"Kg, devo comer "+(yury.GetPeso()*0.05)+"g por dia",QuantoDevoComer);
         }
+
+        [TestMethod]
+        public void Cachorro_Set_Get_Nome_Test()
+        {
+            Cachorro leia = new Cachorro();
+            leia.SetNome("Leya");
+            string novoNome = "O nome do cachorro agora é " + leia.GetNome();
+            Console.WriteLine(novoNome);
+            Assert.AreEqual("O nome do cachorro agora é "+leia.GetNome(), novoNome);
+
+
+        }
+
+        [TestMethod]
+        public void Cachorro_Set_Get_Sexo_Test()
+        {
+            Cachorro leia = new Cachorro();
+            leia.SetSexo("Femea");
+            string novoSexo = "O sexo do cachorro agora é " + leia.GetSexo();
+            Console.WriteLine(novoSexo);
+            Assert.AreEqual("O sexo do cachorro agora é "+leia.GetSexo(), novoSexo);
+
+
+        }
+
+        [TestMethod]
+        public void Cachorro_Set_Get_Raca_Test()
+        {
+            Cachorro leia = new Cachorro();
+            leia.SetRaca("Vira Lata");
+            string novaRaca = "A raça do cachorro agora é " + leia.GetRaca();
+            Console.WriteLine(novaRaca);
+            Assert.AreEqual("A raça do cachorro agora é "+leia.GetRaca(), novaRaca);
+
+
+        }
+
+        [TestMethod]
+        public void Cachorro_Set_Get_Porte_Test()
+        {
+            Cachorro leia = new Cachorro();
+            leia.SetPorte("Pequeno");
+            string novoPorte = "O porte do cachorro agora é " + leia.GetPorte();
+            Console.WriteLine(novoPorte);
+            Assert.AreEqual("O porte do cachorro agora é "+leia.GetPorte(), novoPorte);
+
+
+        }
+
+        [TestMethod]
+        public void Cachorro_Set_Get_Idade_Test()
+        {
+            Cachorro leia = new Cachorro();
+            leia.SetIdade(2);
+            string novaIdade = "A idade do cachorro agora é " + leia.GetIdade();
+            Console.WriteLine(novaIdade);
+            Assert.AreEqual("A idade do cachorro agora é "+leia.GetIdade(), novaIdade);
+
+
+        }
+
+        [TestMethod]
+        public void Cachorro_Set_Get_Peso_Test()
+        {
+            Cachorro leia = new Cachorro();
+            leia.SetPeso(1);
+            string novoPeso = "O peso do cachorro agora é " + leia.GetPeso();
+            Console.WriteLine(novoPeso);
+            Assert.AreEqual("O peso do cachorro agora é "+leia.GetPeso(), novoPeso);
+
+
+        }
+
     }
 }
