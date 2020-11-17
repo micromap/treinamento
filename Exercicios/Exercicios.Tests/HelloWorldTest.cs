a7244d2c8b23e7c9ab1bb5f42790c2af3eecee08
ambalielo <70974623+ambalielo@users.noreply.github.com>
2020-11-17 16:35:56 -0300

    Aula.016

--- Exercicios/Exercicios.Tests/HelloWorldTest.cs
+++ Exercicios/Exercicios.Tests/HelloWorldTest.cs
@@ -113,5 +113,75 @@ namespace Exercicios.Tests
             Assert.AreEqual(cachorro1, cachorro2);
         }
 
+        [TestMethod]
+        public void Tipos_Explicitos_Test()
+        {
+            string nome = "Leia";
+            int idade = 1;
+            double peso = 1.3;
+            Cachorro cachorro = new Cachorro();
+
+            Assert.AreEqual(typeof(string), nome.GetType());
+            Assert.AreEqual(typeof(int), idade.GetType());
+            Assert.AreEqual(typeof(double), peso.GetType());
+            Assert.AreEqual(typeof(Cachorro), cachorro.GetType());
+        }
+
+        [TestMethod]
+        public void Tipos_Implicitos_Test()
+        {
+            var nome = "Leia";
+            var idade = 1;
+            var peso = 1.3;
+            var cachorro = new Cachorro();
+
+            Assert.AreEqual(typeof(string), nome.GetType());
+            Assert.AreEqual(typeof(int), idade.GetType());
+            Assert.AreEqual(typeof(double), peso.GetType());
+            Assert.AreEqual(typeof(Cachorro), cachorro.GetType());
+        }
+
+        [TestMethod]
+        public void Declarar_Sem_Inicializar_Test()
+        {
+            Cachorro cachorro;
+            cachorro = new Cachorro();
+            Assert.AreEqual(typeof(Cachorro), cachorro.GetType());
+
+            // Não podemos declarar variaveis implicitas sem atribuir valor
+            //var nome;
+            //nome = "Leia";
+            //Assert.AreEqual(typeof(string), nome.GetType());
+        }
+
+        [TestMethod]
+        public void Tipos_Que_Aceitam_Null_Test()
+        {
+            string nome = null;
+            Cachorro cachorro = null;
+
+            Assert.AreEqual(null, nome);
+            Assert.AreEqual(null, cachorro);
+
+            // O Tipo int , double, bool não aceitam ser inicializado com o valor nulo
+            // var nome = null;  não podemos atribuir o valor nulo, pois o comando var necessita saber um valor verdadeiro para saber o seu tipo (string,int, double,bool......
+            // int idade = null;       
+            // double peso = null;
+            // Assert.AreEqual(null, idade);
+            // Assert.AreEqual(null, double);
+        }
+
+        [TestMethod]
+        public void Tipos_Nulaveis_Test()
+        {
+            // O Tipo int , double, bool não aceitam ser inicializado com o valor nulo, a menos que você coloque o sinal de ?
+            int? idade = null;
+            double? peso = null;
+            bool? vacinado = null;
+
+            Assert.AreEqual(null, idade);
+            Assert.AreEqual(null, peso);
+            Assert.AreEqual(null, vacinado);
+        }
     }
 }
