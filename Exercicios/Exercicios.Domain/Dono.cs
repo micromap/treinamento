6064660ba2967aed12f1812fec7e8099bb6f5f88
TamaraAlessandra <tamara@gemmap.com.br>
2020-11-20 12:02:08 -0300

    associação de coleções

--- Exercicios/Exercicios.Domain/Dono.cs
+++ Exercicios/Exercicios.Domain/Dono.cs
@@ -1,4 +1,7 @@
 ﻿
+using System;
+using System.Collections.Generic;
+
 namespace Exercicios.Domain
 {
     public class Dono
@@ -6,5 +9,35 @@ namespace Exercicios.Domain
         public string Nome { get; set;}
         public string Telefone { get; set;}
         public string Email { get; set; }
+        public List<Cachorro> Pets { get; set; }
+
+        public void AddPet(Cachorro pet)
+        {
+            if (Pets == null)
+                Pets = new List<Cachorro>();
+
+            Pets.Add(pet);
+            pet.Dono = this;
+        }
+
+        public void RemovePet(Cachorro pet)
+        {
+            if (Pets == null)
+                return;
+
+            if (Pets.Remove(pet))
+                pet.Dono = null;
+        }
+
+        public void AddPet(params Cachorro[] pets) //params interpreta os dois parâmetros, sem precisar de array
+        {
+            foreach (var pet in pets)
+                AddPet(pet);
+        }
+        public void RemovePet(params Cachorro[] pets) 
+        {
+            foreach (var pet in pets)
+                RemovePet(pet);
+        }
     }
 }
---
+++ Exercicios/Exercicios.Tests/DonoTest.cs
@@ -0,0 +1,92 @@
+﻿using Exercicios.Domain;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Exercicios.Tests
+{
+    [TestClass]
+    public class DonoTest
+    {
+        [TestMethod]
+        public void Dono_AddPet_Test()
+        {
+            var leia = new Cachorro { Nome = "Léia" };
+            var yuri = new Cachorro { Nome = "Yuri" };
+
+            var tamara = new Dono { Nome = "Tamara" };
+
+            tamara.AddPet(leia);
+            tamara.AddPet(yuri);
+
+            Assert.AreEqual(2, tamara.Pets.Count);
+            Assert.AreEqual(tamara, leia.Dono);
+            Assert.AreEqual(tamara, yuri.Dono);
+
+            foreach (var pet in tamara.Pets)
+                Console.WriteLine(pet.Nome);
+        }
+
+        [TestMethod]
+        public void Dono_RemovePet_Test()
+        {
+            var leia = new Cachorro { Nome = "Léia" };
+            var yuri = new Cachorro { Nome = "Yuri" };
+
+            var tamara = new Dono { Nome = "Tamara" };
+
+            tamara.AddPet(leia);
+            tamara.AddPet(yuri);
+
+            tamara.RemovePet(yuri);
+
+            Assert.AreEqual(1, tamara.Pets.Count);
+            Assert.AreEqual(tamara, leia.Dono);
+            Assert.AreEqual(null, yuri.Dono);
+
+            foreach (var pet in tamara.Pets)
+                Console.WriteLine(pet.Nome);
+        }
+
+        [TestMethod]
+        public void Dono_AddPets_Test()
+        {
+            var leia = new Cachorro { Nome = "Léia" };
+            var yuri = new Cachorro { Nome = "Yuri" };
+
+            var tamara = new Dono { Nome = "Tamara" };
+
+            tamara.AddPet(leia, yuri);
+
+            Assert.AreEqual(2, tamara.Pets.Count);
+            Assert.AreEqual(tamara, leia.Dono);
+            Assert.AreEqual(tamara, yuri.Dono);
+
+            foreach (var pet in tamara.Pets)
+                Console.WriteLine(pet.Nome);
+        }
+
+        [TestMethod]
+        public void Dono_RemovePets_Test()
+        {
+            var leia = new Cachorro { Nome = "Léia" };
+            var yuri = new Cachorro { Nome = "Yuri" };
+
+            var tamara = new Dono { Nome = "Tamara" };
+
+            var pets = new[] { leia, yuri };
+
+            tamara.AddPet(leia);
+            tamara.AddPet(yuri);
+
+            tamara.RemovePet(yuri, leia);
+
+            Assert.AreEqual(0, tamara.Pets.Count);
+            Assert.AreEqual(null, leia.Dono);
+            Assert.AreEqual(null, yuri.Dono);
+
+            foreach (var pet in tamara.Pets)
+                Console.WriteLine(pet.Nome);
+        }
+    }
+}
