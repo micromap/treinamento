50c7f639b6d3afbb9d8a1fea112cf2493deab3f9
DiegoGemmap <70973775+DiegoGemmap@users.noreply.github.com>
2020-11-20 09:05:47 -0300

    Revert "Revert "Aula 23""
    This reverts commit 8e43e6d4e0a23de9ea5f2450f2959fb63593f8b9.

--- Exercicios/Exercicios.Domain/Dono.cs
+++ Exercicios/Exercicios.Domain/Dono.cs
@@ -9,10 +9,30 @@ namespace Exercicios.Domain
         public string Nome { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
+        public List<Cachorro> Pets { get; set; }
 
         public override string ToString()
         {
             return "Nome: " + Nome + " " + "Email: " + Email + " " + " Telefone: " + Telefone;
+        }
+
+        public void AddPet(Cachorro pet)
+        {
+            if (Pets == null)
+                Pets = new List<Cachorro>();
+
+            Pets.Add(pet);
+            pet.Dono = this;
+
+        }
+
+        public void RemovePet(Cachorro pet)
+        {
+            if (Pets == null)
+                return;
+
+            if (Pets.Remove(pet))
+                pet.Dono = null;
         }
     }
 }
