0d989a0724fc8125297f89fbf793babb199a1586
Rodrigo Sendin <rodrigo@algorama.com.br>
2020-11-23 11:48:54 -0300

    Aula: 026.1 - Herança - Solução dos Exercícios

--- Exercicios/Exercicios.Domain/Gato.cs
+++ Exercicios/Exercicios.Domain/Gato.cs
@@ -1,6 +1,17 @@
-﻿namespace Exercicios.Domain
+﻿using System;
+
+namespace Exercicios.Domain
 {
     public class Gato : Animal, IPet
     {
+        public string Miar(int qtdMiados)
+        {
+            var miados = "";
+
+            for (var i = 1; i <= qtdMiados; i++)
+                miados += "Miau! ";
+
+            return miados.TrimEnd();
+        }
     }
 }
---
+++ Exercicios/Exercicios.Tests/GatoTest.cs
@@ -0,0 +1,43 @@
+﻿using Exercicios.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Exercicios.Tests
+{
+    [TestClass]
+    public class GatoTest
+    {
+        [TestMethod]
+        public void Gato_Miar_Test()
+        {
+            var vesgo = new Gato();
+            var miado = vesgo.Miar(3);
+
+            Console.WriteLine(miado);
+
+            Assert.AreEqual("Miau! Miau! Miau!", miado);
+        }
+
+        [TestMethod]
+        public void Gato_Validar_Test()
+        {
+            try
+            {
+                var gato = new Gato
+                {
+                    Nome = ""
+                };
+
+                gato.Validar();
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                var ok = ex.Message.Contains("Nome do Pet é Obrigatório!");
+
+                Assert.AreEqual(true, ok);
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
