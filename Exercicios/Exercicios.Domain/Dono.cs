0a52489d83fcb4f18cd6dee7e53cc180e1043058
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-11-26 16:25:26 -0300

    022.1 - Associações - Solução dos Exercícios

---
+++ Exercicios/Exercicios.Domain/Dono.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercicios.Domain
+{
+    public class Dono
+    {
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+
+    }
+}
