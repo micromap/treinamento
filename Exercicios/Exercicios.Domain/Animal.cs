2da599e3f9decb1f6bd1c78ea91480541c094cca
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-12-04 16:42:23 -0300

    028 - Classes e Membros Estáticos

--- Exercicios/Exercicios.Domain/Animal.cs
+++ Exercicios/Exercicios.Domain/Animal.cs
@@ -42,14 +42,9 @@ namespace Exercicios.Domain
         {
             var mensagens = ValidacoesComuns();
 
-            if (mensagens.Count > 0)
-            {
-                var exceptionMessage = "";
-                foreach (var msg in mensagens)
-                    exceptionMessage += msg + Environment.NewLine;
-
-                throw new Exception(exceptionMessage);
-            }
+            var ex = Helpers.ConvertStringListToException(mensagens);
+            if (ex != null)
+                throw ex;
         }
     }
 }
---
+++ Exercicios/Exercicios.Domain/Helpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicios.Domain
+{
+    public static class Helpers
+    {
+        public static Exception ConvertStringListToException(List<string> mensagens)
+        {
+            var exceptionMessage = "";
+            foreach (var msg in mensagens)
+                exceptionMessage += msg + Environment.NewLine;
+
+            return string.IsNullOrWhiteSpace(exceptionMessage)
+                ? null
+                : new Exception(exceptionMessage);
+        }
+    }
+}
