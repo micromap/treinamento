2b3a83e6456b87968e976e95be5ad21a0652417b
TamaraAlessandra <tamara@gemmap.com.br>
2020-12-11 16:52:41 -0300

    Aula07Sergio

---
+++ Aula07Sergio/Aula07Sergio/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula07Sergio
+{
+    class Produto
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
---
+++ Aula07Sergio/Aula07Sergio/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula07Sergio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Produto();
+            p1.Codigo = 1;
+            p1.Nome = "Coca Cola";
+            p1.Preco = 12.50;
+            p1.Quantidade = 15;
+        }
+    }
+}
