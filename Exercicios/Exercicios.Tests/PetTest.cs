debb1499c803aef2dc621e9d361152bab0356c80
ambalielo <70974623+ambalielo@users.noreply.github.com>
2020-12-30 11:58:16 -0300

    aula 031 - LINQ

--- Exercicios/Exercicios.Tests/PetTest.cs
+++ Exercicios/Exercicios.Tests/PetTest.cs
@@ -2,24 +2,158 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Exercicios.Tests
 {
     [TestClass]
     public class PetTest
     {
+        private static List<IPet> _pets;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            // carrega a lista de pets pelo arquivo C:\\Aula030\\pets.csv
+            _pets = new List<IPet>();
+            _pets.CarregaPetsDoArquivo("C:\\Aula030\\pets.csv");
+        }
+
+        private static void ImprimePets(IEnumerable<IPet> pets)
+        {
+            foreach (var pet in pets)
+                Console.WriteLine($"[{pet.GetTipo()}] {pet.Nome} - {pet.Dono.Nome}");
+        }
+
         [TestMethod]
         public void CarregaPetsDoArquivo_Test()
         {
-            var pets = new List<IPet>();
+            Assert.AreEqual(15, _pets.Count);
 
-            // carrega a lista de pets pelo arquivo C:\\Aula030\\pets.csv
-            pets.CarregaPetsDoArquivo("C:\\Aula030\\pets.csv");
+            ImprimePets(_pets);
+        }
 
-            Assert.AreEqual(15, pets.Count);
+        [TestMethod]
+        public void Linq_Test()
+        {
+            // LINQ é um acrônimo para Language Integrated Query, ou Consulta Integrada à Linguagem . 
+            // Trata - se de um “framework” dentro do .NET destinado a auxiliar os desenvolvedores a escrever 
+            // expressões de consulta diretamente em C# de maneira agnóstica.
+            var query = from pet in _pets
+                        where pet.GetTipo() == "Cachorro"
+                        select pet;
 
-            foreach (var pet in pets)
-                Console.WriteLine($"[{pet.GetTipo()}] {pet.Nome} - {pet.Dono.Nome}");
+            ImprimePets(query);
+        }
+
+        [TestMethod]
+        public void Linq_Metodos_Test()
+        {
+            var query = _pets.Where(pet => pet.GetTipo() == "Gato");
+            ImprimePets(query);
+        }
+
+        [TestMethod]
+        public void Linq_Order_Test()
+        {
+            var query = from pet in _pets
+                        where pet.GetTipo() == "Cachorro"
+                        orderby pet.Nome
+                        select pet;
+
+            ImprimePets(query);
+        }
+
+        [TestMethod]
+        public void Linq_Order_Metodo_Test()
+        {
+            var query = _pets.Where(pet => pet.GetTipo() == "Gato")
+                             .OrderBy(pet => pet.Nome);
+
+            ImprimePets(query);
+        }
+
+        [TestMethod]
+        public void Linq_Projecao_Test()
+        {
+            var query = from pet in _pets
+                        where pet.GetTipo() == "Cachorro"
+                        orderby pet.Peso descending
+                        select new { Nome = pet.Nome, Peso = pet.Peso };
+
+            foreach (var pet in query)
+                Console.WriteLine($"{pet.Nome} - {pet.Peso}");
+        }
+
+        [TestMethod]
+        public void Linq_Projecao_Metodo_Test()
+        {
+            var query = _pets.Where(pet => pet.GetTipo() == "Gato")
+                             .OrderByDescending(pet => pet.Peso)
+                             .Select(pet => new { pet.Nome, pet.Peso });
+
+            foreach (var pet in query)
+                Console.WriteLine($"{pet.Nome} - {pet.Peso}");
+        }
+
+        [TestMethod]
+        public void Linq_Skip_Take_Test()
+        {   
+            // este metodo serve para fazer paginação
+            // Take(3) - pegar os três primeiros da query
+            // Skip(3) - pular os três primeiros da query
+            var query = (from pet in _pets
+                         where pet.GetTipo() == "Cachorro"
+                         orderby pet.Peso descending
+                         select new { Nome = pet.Nome, Peso = pet.Peso }).Skip(3).Take(3);
+
+            foreach (var pet in query)
+                Console.WriteLine($"{pet.Nome} - {pet.Peso}");
+        }
+
+        [TestMethod]
+        public void Linq_First_Last_Test()
+        {
+            var query = from pet in _pets
+                        where pet.GetTipo() == "Cachorro"
+                        orderby pet.Peso descending
+                        select new { Nome = pet.Nome, Peso = pet.Peso };
+
+            var primeiro = query.FirstOrDefault();
+            var ultimo = query.LastOrDefault();
+
+            Console.WriteLine("Primeiro: "+primeiro.Nome);
+            Console.WriteLine("Último: " + ultimo.Nome);
+        }
+
+        [TestMethod]
+        public void Linq_Agregacoes_Test()
+        {
+            var query = from pet in _pets
+                        where pet.GetTipo() == "Cachorro"
+                        orderby pet.Peso descending
+                        select new { Nome = pet.Nome, Peso = pet.Peso };
+
+            var soma = query.Sum(pet => pet.Peso);
+            var maior = query.Max(pet => pet.Peso);
+            var menor = query.Min(pet => pet.Peso);
+            var media = query.Average(pet => pet.Peso);
+
+            Console.WriteLine($"Soma Peso: {soma}");
+            Console.WriteLine($"Maior Peso: {maior}");
+            Console.WriteLine($"Menor Peso: {menor}");
+            Console.WriteLine($"Média Peso: {media}");
+        }
+
+        [TestMethod]
+        public void Linq_Group_Test()
+        {
+            var query = from pet in _pets
+                        group pet by pet.Sexo into g
+                        select new { Sexo = g.Key, Total = g.Count() };
+
+            foreach (var pet in query)
+                Console.WriteLine($"{pet.Sexo} - {pet.Total}");
         }
     }
 }
